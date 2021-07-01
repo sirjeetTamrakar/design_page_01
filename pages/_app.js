5ae7b0331d6ed65e555efbdd3e7d68fd2ea3ebcc
Srijeet-Tamrakar <tsirjeet220@gmail.com>
2021-07-01 12:34:08 +0545

    design

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import React, {useEffect} from 'react';
 function MyApp({ Component, pageProps })
 {
   useEffect(() => {
-		AOS.init({delay: 450, offset: 60, duration: 1000});
+		AOS.init({delay: 450, offset: 60, duration: 800});
 	}, []);
   return <Component {...pageProps} />
 }
